aba2ef5211e21e259797c0271e0fc2932cbc5ed9
Nate Denys <nate_denys@yahoo.com>
2021-08-14 12:26:06 -0700

    Adding Checkbox functionality and Computed Styling
    Used a ternary operator to conditionally change the styling of completed todo items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
 import ToDoItem from './ToDoItem'
-import todosData from './todosData';
+import todosData from './todosData'
+import React from "react"
 
-function App() {
-  const todos = todosData.map(todo => <ToDoItem key={todo.id} todo={todo} />)
-  return (
-    <div className="todo-list">
-      {todos}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
+  render() {
+    const todos = this.state.todos.map(todo => <ToDoItem key={todo.id} todo={todo} 
+      handleChange={this.handleChange}/>)
+    return (
+      <div className="todo-list">
+        {todos}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function ToDoItem(props) {
+    const completedStyle = {
+        fontStyle: 'italic',
+        color: '#aaa',
+        textDecoration: 'line-through'
+    }
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.todo.completed}/>
-            <p>{props.todo.text}</p>
+            <input 
+                type="checkbox" 
+                checked={props.todo.completed} 
+                onChange={() => props.handleChange(props.todo.id)}
+            />
+            <p style={props.todo.completed ? completedStyle: null}>{props.todo.text}</p>
         </div>
     )
 }
